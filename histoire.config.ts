392870b2b5d60cd211bc33d0117a582570d9c0cd
David McKeone <david@artsman.com>
2025-05-29 08:24:46 -0600

    Re-add @heroIcons and make them compatible with Histoire

--- histoire.config.ts
+++ histoire.config.ts
@@ -3,5 +3,8 @@ import { HstVue } from "@histoire/plugin-vue"
 
 export default defineConfig({
   plugins: [HstVue()],
-  setupFile: "./src/histoire.ts"
+  setupFile: "./src/histoire.ts",
+  // DEV NOTE: If you *do not* use viteNodeInlineDeps then this issue could occur:
+  //           https://github.com/histoire-dev/histoire/issues/696
+  viteNodeInlineDeps: [/@heroicons.+/]
 })
--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed } from "vue"
+import { ArrowUpIcon, ArrowDownIcon } from "@heroicons/vue/20/solid"
 
 export interface Stat {
   name: string
@@ -109,6 +110,16 @@ console.log(displayStats.value)
                 'text-red-800': item.changeType === ChangeType.Decrease
               }"
             >
+              <ArrowUpIcon
+                v-if="item.changeType === ChangeType.Increase"
+                class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500"
+                aria-hidden="true"
+              />
+              <ArrowDownIcon
+                v-else-if="item.changeType === ChangeType.Decrease"
+                class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
+                aria-hidden="true"
+              />
               <span v-if="item.changeType === ChangeType.Increase" class="sr-only">
                 Increase by
               </span>
